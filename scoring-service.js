a1a4e5e622568352f8725b7d272e9cf3cf8c4393
Ricardo Baeza <baezaric3600@gmail.com>
2018-11-17 09:24:42 -0700

    made it so that you could grab any golf course and display it to the page

--- scoring-service.js
+++ scoring-service.js
@@ -11,12 +11,13 @@ function addPlayers() {
         // add players
         for (let i = 1; i <= numbPlayers; i++) {
             $(".in-score").append("<div class='score-container'><div class='player-container'>player" + i + "</div></div>");
-            $('.out-score').append("<div class='score-container'></div>")
+            $('.out-score').append("<div class='os-score-container'></div>")
         // adds the hole count at the top
         }
-        for (let i = 0; i <= 9; i++) {
+        for (let i = 1; i <= 9; i++) {
             $('.hole-container').append('<div class="hole">' + i + '</div>');
         }
+
 
         $('.hole-container').append('<div class="hole">Out</div>');
 
@@ -24,10 +25,15 @@ function addPlayers() {
 
 
         // this will append to the players
-        for (let i = 0; i <= 10; i++) {
+        for (let i = 0; i <= 9; i++) {
             $('.score-container').append('<input class="input-hole" type="text">')
 
         }
+        for (let i = 10; i <= 20; i++) {
+            $('.os-score-container').append('<input class="input-hole" type="text">')
+        }
+
+
 
         for (let i = 10; i <= 18; i++) {
             $('.OS-hole-container').append('<div class="hole">' + i + '</div>');
@@ -49,21 +55,21 @@ function teeCourseSelect() {
     $('.remove-tee').remove();
     switch (document.getElementById('course-select').value) {
         case '19002':
-            $('#tee-type').append('<option class="remove-tee" value="2">champion</option>');
-            $('#tee-type').append('<option class="remove-tee" value="3">mens</option>');
-            $('#tee-type').append('<option class="remove-tee" value="4">womans</option>');
+            $('#tee-type').append('<option class="remove-tee" value="0">champion</option>');
+            $('#tee-type').append('<option class="remove-tee" value="1">mens</option>');
+            $('#tee-type').append('<option class="remove-tee" value="2">womans</option>');
             break;
         case '11819':
-            $('#tee-type').append('<option class="remove-tee" value="1">pro</option>');
-            $('#tee-type').append('<option class="remove-tee" value="2">champion</option>');
-            $('#tee-type').append('<option class="remove-tee" value="3">mens</option>');
-            $('#tee-type').append('<option class="remove-tee" value="4">womans</option>');
+            $('#tee-type').append('<option class="remove-tee" value="0">pro</option>');
+            $('#tee-type').append('<option class="remove-tee" value="1">champion</option>');
+            $('#tee-type').append('<option class="remove-tee" value="2">mens</option>');
+            $('#tee-type').append('<option class="remove-tee" value="3">womans</option>');
             break;
         case '18300':
-            $('#tee-type').append('<option class="remove-tee" value="1">pro</option>');
-            $('#tee-type').append('<option class="remove-tee" value="2">champion</option>');
-            $('#tee-type').append('<option class="remove-tee" value="3">mens</option>');
-            $('#tee-type').append('<option class="remove-tee" value="4">womans</option>');
+            $('#tee-type').append('<option class="remove-tee" value="0">pro</option>');
+            $('#tee-type').append('<option class="remove-tee" value="1">champion</option>');
+            $('#tee-type').append('<option class="remove-tee" value="2">mens</option>');
+            $('#tee-type').append('<option class="remove-tee" value="3">womans</option>');
             break;
 
         default:
@@ -75,22 +81,43 @@ function teeCourseSelect() {
 
 
 function getCourseInfo(value) {
-    document.getElementById('tee-type').value = teeTypeValue;
     let xhttp = new  XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             mycourse = JSON.parse(this.responseText);
             document.getElementById('blank').innerHTML = mycourse.data.name;
             console.log(mycourse);
-            for (let i = 0; i <= 10; i++) {
-                // this needs to be changed right now its just a place holder
-                $('.yards-container').append('<div class= info-space> null</div>')
+            // adding the yards to the golf card depending on the type of tee
+
+            for (let i = 0; i <= 8; i++) {
+                $('.yards-container').append('<div class= info-space>' + mycourse.data.holes[i].teeBoxes[document.getElementById('tee-type').value].yards + '</div>')
             }
+
+            for (let i = 9; i <= 17; i++) {
+                $('.os-yards-container').append('<div class= info-space>' + mycourse.data.holes[i].teeBoxes[document.getElementById('tee-type').value].yards + '</div>')
+            }
+
+            for (let i = 0; i <= 8; i++) {
+                $('.par-container').append('<div class= info-space>' + mycourse.data.holes[i].teeBoxes[document.getElementById('tee-type').value].par + '</div>')
+            }
+
+            for (let i = 9; i <= 17; i++) {
+                $('.os-par-container').append('<div class= info-space>' + mycourse.data.holes[i].teeBoxes[document.getElementById('tee-type').value].par + '</div>')
+            }
+
+            for (let i = 0; i <= 8; i++) {
+                $('.hcp-container').append('<div class= info-space>' + mycourse.data.holes[i].teeBoxes[document.getElementById('tee-type').value].hcp + '</div>')
+            }
+            for (let i = 9; i <= 17; i++) {
+                $('.os-hcp-container').append('<div class= info-space>' + mycourse.data.holes[i].teeBoxes[document.getElementById('tee-type').value].hcp + '</div>')
+            }
+
         }
     };
     xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses/"+ value , true );
     xhttp.send();
 }
+
 
 
 
